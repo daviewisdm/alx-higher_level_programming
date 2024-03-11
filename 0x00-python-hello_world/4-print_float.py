5fc91ad660b364a7854af69186acf6cc19934dcf
Davie Kibet <wisdomdavie24@gmail.com>
2024-03-11 06:22:00 -0700

    0x00 Hello World

---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+number = 3.14159
+# your code goes here
+print(f"Float: {number:.2f}")
+
---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+word = "object-oriented programming with Python"
+print(word[:10] + word[10:20] + word[20:])
+
