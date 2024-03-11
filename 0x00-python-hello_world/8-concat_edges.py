d4d3575a85ccac2bd3b822516813b9e1a522edb7
Davie Kibet <wisdomdavie24@gmail.com>
2024-03-11 06:30:13 -0700

    Push

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-word = "object-oriented programming with Python"
-print(word[:10] + word[10:20] + word[20:])
-
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
