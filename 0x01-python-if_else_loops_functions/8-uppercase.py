ae63f4bce165edb6fc820219e394f0bdd85cb05d
Davie Kibet <wisdomdavie24@gmail.com>
2024-03-12 11:33:19 -0700

    Pushie

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+	for i in range(0, len(str)):
+		s = ord(str[i])
+		if s >= 97 and s <= 122:
+			s = s - 32
+			print("{:c}".format(s), end="")
+		print('')
---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+* insert_node - inserts number into a sorted singly linked list
+* @head: linked list
+* @number: value to be assigned to new node
+* Return: address of new node or NULL if fail
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node = NULL;
+	listint_t *current = NULL;
+	current = *head;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = number;
+	new_node->next = NULL;
+	if (*head == NULL || new_node->n < current->n)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	while (current->next != NULL && (current->n < new_node->n)
+			&& ((current->next)->n < new_node->n))
+		current = current->next;
+	new_node->next = current->next;
+	current->next = new_node;
+	return (new_node);
+}
