ff6c527bd239650163b97a888c1afda703203bb2
Davie Kibet <wisdomdavie24@gmail.com>
2024-03-20 16:07:13 -0700

    UPDATED map square, ERR!

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_map(matrix=[]):
-    return (list(map(lambda row: list(map(lambda x: x**2, row)), matrix)))
+    return list(map(list, map(lambda x: x**2, *matrix)))
 
