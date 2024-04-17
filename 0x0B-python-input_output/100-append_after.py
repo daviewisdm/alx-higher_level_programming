88a5858ace1887e9167b7be92666256bc9857d3e
Davie Kibet <wisdomdavie24@gmail.com>
2024-04-17 13:48:37 -0700

    push updates

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -2,18 +2,20 @@
 
 def append_after(filename="", search_string="", new_string=""):
     # Temporary file to store the modified content
-    temp_filename = filename + ".tmp"
-    
-    with open(filename, 'r') as file, open(temp_filename, 'w') as temp_file:
-        for line in file:
-            # Write the current line to the temporary file
-            temp_file.write(line)
-            # If the line contains the search string, append the new string
-            if search_string in line:
-                temp_file.write(new_string)
-    
-    # Replace the original file with the modified content
-    import os
-    os.remove(filename)
-    os.rename(temp_filename, filename)
-
+    lines_list = list()
+    line = ""
+    # open file for reading
+    with open(filename, mode='r', encoding='utf-8') as f:
+        # read line by line till end
+        line = f.readline()
+        while line != "":
+            # append read line to lines_list and
+            # find search-string in current line
+            lines_list.append(line)
+            if line.find(search_string) != -1:
+                lines_list.append(new_string)
+            # read next line
+            line = f.readline()
+        # end of file, write to file and exit
+    with open(filename, mode='w', encoding='utf-8') as file_wr:
+        file_wr.writelines(lines_list)
