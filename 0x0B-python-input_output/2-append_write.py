f78cf26fcfa6687113d4a3a5ae0dd1cfcd87ba85
Davie Kibet <wisdomdavie24@gmail.com>
2024-04-17 13:40:54 -0700

    updated 2 append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,15 @@
-2-append_write.py
+#!/usr/bin/python3
+"""task 2 module"""
+
+
+def append_write(filename="", text=""):
+    """writes text to a file, must append text if file exists
+    Args:
+        filename (str) name of file to be read
+        text (str) text to be written
+    function should:
+        create the file if doesn’t exist
+    """
+
+    with open(filename, mode='a', encoding='utf-8') as f_file:
+        return f_file.write(text)
